e593fe031d54bf9cdcede04d4cb152507912c9fd
Alex <sederquest.alex@gmail.com>
2020-10-08 20:56:20 -0400

    added includes

--- shell-with-builtin.c
+++ shell-with-builtin.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
 #include <glob.h>
 #include <signal.h>
 #include "sh.h"
